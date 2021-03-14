72af56a4a268047f6801935a9031b957fcc81eb9
indradarham <indraramadhan201299@gmail.com>
2021-03-14 15:56:58 +0800

    commit pertama Muhammad Indra Ramadha  17630105

---
+++ profile.php
@@ -0,0 +1,49 @@
+<div class="row">
+	<div class="col-md-12">
+		<div class="text-center">
+			<img src="img/indra.jpg" alt="Indra Darham" width="160" class="rounded-circle">
+			<table class="table mt-4" style="width:100%; margin:0 auto;">
+				<tbody>
+					<tr>
+						<td align="right" width="50%"><strong>NPM:</strong></td>
+						<td align="left" width="50%">17630105</td>
+					</tr>
+					<tr>
+						<td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+						<td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+					</tr>
+					<tr>
+						<td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+						<td align="left" width="50%">Muhammad Indra Ramadhan</td>
+					</tr>
+					<tr>
+						<td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+						<td align="left" width="50%">Desember, 20 Desember 1999</td>
+					</tr>
+					<tr>
+						<td align="right" width="50%"><strong>Telepon :</strong></td>
+						<td align="left" width="50%">0858 4991 2546</td>
+					</tr>
+					<tr>
+						<td align="right" width="50%"><strong>email :</strong></td>
+						<td align="left" width="50%">indraramadhan201299@gmail.com@gmail.com</td>
+					</tr>
+					<tr>
+						<td align="right" width="50%"><strong>Media Sosial :</strong></td>
+						<td align="left" width="50%">
+							<a href="https://www.facebook.com/muhammad.i.ramadhan.7" target="_blank" style="text-decoration:none;">
+								<img src="img/medsos/facebook.svg" width="30" alt="facebook">
+							</a>
+							<a href="https://www.instagram.com/indradarham_/" target="_blank" style="text-decoration:none;">
+								<img src="img/medsos/instagram.svg" width="30" alt="instagram">
+							</a>
+							<a href="http://twitter.com/indradarham2" target="_blank" style="text-decoration:none;">
+								<img src="img/medsos/twitter.svg" width="30" alt="twitter">
+							</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
